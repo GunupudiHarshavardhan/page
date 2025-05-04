53b2d0f2485acb9cd0392dcd62c5953f2ad0b08f
GUNUPUDI HARSHAVARDHAN <94680046+GunupudiHarshavardhan@users.noreply.github.com>
2025-05-04 22:48:04 +0530

    Update profile.js

--- pages/profile.js
+++ pages/profile.js
@@ -1,18 +1,1 @@
-// pages/profile.js
 
-
-function Profile() {
-  const { user } = useContext(AuthContext);
-
-  return (
-    <div className="min-h-screen flex flex-col items-center justify-center">
-      <div className="bg-white p-6 rounded shadow-md w-96">
-        <h2 className="text-2xl font-semibold mb-4">User Profile</h2>
-        <p><strong>Name:</strong> {user?.name}</p>
-        <p><strong>Email:</strong> {user?.email}</p>
-      </div>
-    </div>
-  );
-}
-
-export default withAuth(Profile);
